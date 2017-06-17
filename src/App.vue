c1eef08bc4f3ce814854c6e4d32e4c6949281ec0
Ali Baran ESer <alibaraneser@gmail.com>
2017-06-17 20:24:04 +0300

    final

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <p v-if="isLoading">Please Wait, loading...</p>
+    <search @SearchRequested="handleSearch"></search>
+    <preview :gifs=gifs></preview>
+  </div>
+</template>
+
+<script>
+
+import Search from './components/Search.vue'
+import Preview from './components/Preview.vue'
+
+export default {
+  name: 'app',
+  components : {Search, Preview},
+    data() {
+        return {
+            gifs: [],
+            isLoading : true
+        }
+    },
+    methods : {
+      doQuery (url) {
+          fetch(url)
+              .then((res) => {return res.json()})
+              .then((res) => {this.gifs = res.data; this.isLoading = false;})
+      },
+      handleSearch(query) {
+          this.gifs = [];
+          this.isLoading = true;
+          const url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC`;
+          this.doQuery(url);
+
+      }
+    },
+    created () {
+      const url = 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC';
+      this.doQuery(url);
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search">
+        <input type="text" v-model="query" @keypress.enter="handleSearch">
+        <button v-on:click="handleSearch">Search</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            query : ''
+        }
+    },
+    methods : {
+        handleSearch () {
+            this.$emit('SearchRequested',this.query);
+        }
+    }
+}
+</script>
+
+<style>
+    .search {
+        margin : 20px;
+    }
+</style>
